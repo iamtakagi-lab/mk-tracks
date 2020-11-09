c24bc063e247c6b4acf13d1d7d49974844eeeffb
riptakagi <smatchjp@gmail.com>
2020-11-10 03:00:40 +0900

    init

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+declare const game: {
+    title_jp: string,
+    title_en: string,
+    title_abbreviation: string,
+    cups: [ 
+        {
+            id: number,
+            name_en: string,
+            name_jp: string,
+            tracks: [
+                {
+                    id: number,
+                    name_en: string,
+                    name_jp: string
+                    abbreviation: string
+                }
+            ]
+        }
+    ]
+}
+
+export default game
